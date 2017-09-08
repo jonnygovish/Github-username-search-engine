294453b41024a8593d7f2b670b9fb8f4eb23b7ba
John <jonnygovish@gmail.com>
2017-09-08 18:09:39 +0300

    updating changes in the script.js

--- js/script.js
+++ js/script.js
@@ -1,15 +1,21 @@
 //Back end logic
 var apiKey = require('./../.env').apiKey;
 
-function Repos(){
-}
+function Repos() {}
 
-Repos.prototype.getRepos = function(userInput){
-  $.get('https://api.github.com/users/' + userInput +'?access_token=' + apiKey).then(function(response){
-    console.log(response);
-  }).fail(function(error){
-    console.log(error.responseJSON.message);
-  });
+Repos.prototype.getRepos = function (userInput) {
+    $.get('https://api.github.com/users/' + userInput + '?access_token=' + apiKey).then(function (response) {
+        console.log(response);
+    }).fail(function (error) {
+        console.log(error.responseJSON.message);
+    });
+    $.get('https://api.github.com/users/' + userInput + '/repos?access_token=' + apiKey).then(function (response) {
+        for (var n = 0; n <= response.length; n++)
+        console.log(response[n].name);
+        console.log(response[n].description)
+    }).fail(function (error) {
+//        console.log(error.responseJSON.message);
+    });
 };
 
 exports.reposModule = Repos;
