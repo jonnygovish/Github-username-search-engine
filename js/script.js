23516695dfef58de66cbbc92e8f3de2257279928
John <jonnygovish@gmail.com>
2017-09-09 17:19:40 +0300

    diplaying the user repos to the page

--- js/script.js
+++ js/script.js
@@ -6,13 +6,13 @@ function Repos() {}
 Repos.prototype.getRepos = function (userInput) {
     $.get('https://api.github.com/users/' + userInput + '?access_token=' + apiKey).then(function (response) {
         console.log(response);
+        $('#output-username').html('<h2 id="user-name">' + response.name + '</h2><img id="user-avatarImage" src=' + response.avatar_url + '>');
     }).fail(function (error) {
-        console.log(error.responseJSON.message);
+        
     });
     $.get('https://api.github.com/users/' + userInput + '/repos?access_token=' + apiKey).then(function (response) {
         for (var n = 0; n <= response.length; n++)
-        console.log(response[n].name);
-        console.log(response[n].description)
+        $('#display-info').append('<li><a target="_blank" href="' + response[n].html_url + '"><p>' + response[n].name +'</p><p>' + response[n].description + '</p></a></li>');
     }).fail(function (error) {
 //        console.log(error.responseJSON.message);
     });
