bd4e43bd494be9f6f5cb46a46fc7fbb82fb2b182
John <jonnygovish@gmail.com>
2017-09-09 18:17:04 +0300

    scripts.js Taking care of the repos with no description

--- js/script.js
+++ js/script.js
@@ -8,13 +8,17 @@ Repos.prototype.getRepos = function (userInput) {
         console.log(response);
         $('#output-username').html('<h2 id="user-name">' + response.name + '</h2><img id="user-avatarImage" src=' + response.avatar_url + '>');
     }).fail(function (error) {
-        
+
     });
     $.get('https://api.github.com/users/' + userInput + '/repos?access_token=' + apiKey).then(function (response) {
         for (var n = 0; n <= response.length; n++)
-        $('#display-info').append('<li><a target="_blank" href="' + response[n].html_url + '"><p>' + response[n].name +'</p><p>' + response[n].description + '</p></a></li>');
+            if (response[n].description === null) {
+                $('#display-info').append('<li><a target="_blank" href="' + response[n].html_url + '"><p>' + response[n].name + '</p><p>This repo doesnt have a description </p></a></li>');
+            } else {
+                $('#display-info').append('<li><a target="_blank" href="' + response[n].html_url + '"><p>' + response[n].name + '</p><p>' + response[n].description + '</p></a></li>');
+            }
     }).fail(function (error) {
-//        console.log(error.responseJSON.message);
+        //        console.log(error.responseJSON.message);
     });
 };
 
