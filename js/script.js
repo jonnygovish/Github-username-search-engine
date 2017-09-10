2a2b2fdd2e924ce15d7b983dc08b61d6f606c1a5
John <jonnygovish@gmail.com>
2017-09-10 14:33:55 +0300

    show errors

--- js/script.js
+++ js/script.js
@@ -7,10 +7,11 @@ function Repos() {
 Repos.prototype.getRepos = function (userInput) {
     $.get('https://api.github.com/users/' + userInput + '?access_token=' + apiKey).then(function (response) {
         console.log(response);
+        $('#results').show();
         $('#output-name').html(response.name);
         $('#output-avatar').html('<img id="user-avatarImage" src=' + response.avatar_url + '>');
     }).fail(function (error) {
-
+        $('#error').show().text("We couldn’t find any users matching "+"'"+ userInput +"'");
     });
     $.get('https://api.github.com/users/' + userInput + '/repos?access_token=' + apiKey).then(function (response) {
         for (var n = 0; n <= response.length; n++)
