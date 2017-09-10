91bc9abec05128df5477cef772110378cba213bb
John <jonnygovish@gmail.com>
2017-09-10 12:59:45 +0300

    script-interface.js displaying the search result after the form has been submitted

--- js/script-interface.js
+++ js/script-interface.js
@@ -1,13 +1,13 @@
 
 var Repos = require('./../js/script.js').reposModule;
-
 $(document).ready(function(){
     var currentReposObject = new Repos();
-    $('#form-group').click(function(event){
+    $('#form-group').submit(function(event){
         event.preventDefault();
         //storing the user input
         var userInput = $('#username').val();
         console.log(userInput);
+        $('#results').show();
         //Displ#y the name
         $('#output-username').text(userInput);
         debugger;
